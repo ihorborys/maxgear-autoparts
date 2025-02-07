45fddae18e3827c80e664d25bec8a842422260c9
ihorborys <ihorborys@gmail.com>
2025-02-08 00:51:59 +0200

    Edit swiper

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,12 +7,17 @@ import 'swiper/css/bundle';
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
+  mousewheel: true,
   slidesPerView: 1,
   spaceBetween: 20,
 
   breakpoints: {
-    1280: {
+    768: {
       slidesPerView: 2, // Від 1280px і більше буде 2 слайди
+    },
+
+    1440: {
+      slidesPerView: 3, // Від 1280px і більше буде 3 слайди
     },
   },
 
