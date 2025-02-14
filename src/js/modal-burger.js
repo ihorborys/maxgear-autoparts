1bd095e1b2701c68d0d16a1a5e6c2148b1f2044a
ihorborys <ihorborys@gmail.com>
2025-02-14 21:44:20 +0200

    Edit burger menu

--- src/js/modal-burger.js
+++ src/js/modal-burger.js
@@ -1,25 +1,34 @@
-// Open burger modal menu
+// // Open burger modal menu
+// const burgerButton = document.querySelector('.mobile-menu-btn');
+// const modalBurger = document.querySelector('.modal-overlay.burger');
+//
+// burgerButton.addEventListener('click', event => {
+//   modalBurger.classList.add('active');
+// });
+
+// // Close burger modal menu
+// const closeButton = document.querySelector('.modal-close-btn.burger');
+//
+// closeButton.addEventListener('click', event => {
+//   modalBurger.classList.remove('active');
+// });
+
+// Open-close burger modal menu 2
 const burgerButton = document.querySelector('.mobile-menu-btn');
-const modalBurger = document.querySelector('.modal-overlay.burger');
+const modalBurger = document.querySelector('.burger-section');
 
 burgerButton.addEventListener('click', event => {
-  modalBurger.classList.add('active');
+  if (modalBurger.classList.contains('active')) modalBurger.classList.remove('active');
+  else modalBurger.classList.add('active');
 });
 
-// Close burger modal menu
-const closeButton = document.querySelector('.modal-close-btn.burger');
-
-closeButton.addEventListener('click', event => {
-  modalBurger.classList.remove('active');
-});
-
-// Select item burger modal menu
-const mobileItems = document.querySelectorAll('.mobile-menu-list-item');
-mobileItems.forEach(item => {
-  item.addEventListener('click', () => {
-    mobileItems.forEach(item => {
-      item.classList.remove('active');
-    });
-    item.classList.add('active');
-  });
-});
+// // Select item burger modal menu
+// const mobileItems = document.querySelectorAll('.mobile-menu-list-item');
+// mobileItems.forEach(item => {
+//   item.addEventListener('click', () => {
+//     mobileItems.forEach(item => {
+//       item.classList.remove('active');
+//     });
+//     item.classList.add('active');
+//   });
+// });
