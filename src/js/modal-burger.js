c5b723335d6678720189526bd2c59b1677468613
ihorborys <ihorborys@gmail.com>
2025-02-19 12:58:28 +0200

    Edit burger menu

--- src/js/modal-burger.js
+++ src/js/modal-burger.js
@@ -1,6 +1,39 @@
-// Open-close burger modal menu
+// // Open burger modal menu
+// const burgerButton = document.querySelector('.mobile-menu-btn');
+// const modalBurger = document.querySelector('.modal-overlay.burger');
+//
+// burgerButton.addEventListener('click', event => {
+//   modalBurger.classList.add('active');
+// });
+
+// // Close burger modal menu
+// const closeButton = document.querySelector('.modal-close-btn.burger');
+//
+// closeButton.addEventListener('click', event => {
+//   modalBurger.classList.remove('active');
+// });
+
+// Open-close burger modal menu 2
 const burgerButton = document.querySelector('.mobile-menu-btn');
 const modalBurger = document.querySelector('.burger-section');
+
+// burgerButton.addEventListener('click', event => {
+//   if (modalBurger.classList.contains('active')) {
+//     modalBurger.classList.remove('active');
+//   } else modalBurger.classList.add('active');
+// });
+
+// burgerButton.addEventListener('click', event => {
+//   if (modalBurger.classList.contains('active')) {
+//     modalBurger.classList.add('hide');
+//     setTimeout(() => {
+//       modalBurger.classList.remove('active');
+//       modalBurger.classList.remove('hide');
+//     }, 1500); // 300ms – підлаштуй під свою анімацію
+//   } else {
+//     modalBurger.classList.add('active');
+//   }
+// });
 
 burgerButton.addEventListener('click', event => {
   if (modalBurger.classList.contains('active')) {
@@ -14,4 +47,16 @@ burgerButton.addEventListener('click', event => {
   } else {
     modalBurger.classList.add('active');
   }
+});
+
+// Select item burger modal menu
+const mobileItems = document.querySelectorAll('.mobile-menu-list-item');
+console.log(mobileItems);
+mobileItems.forEach(item => {
+  item.addEventListener('click', () => {
+    mobileItems.forEach(item => {
+      item.classList.remove('active');
+    });
+    item.classList.add('active');
+  });
 });
